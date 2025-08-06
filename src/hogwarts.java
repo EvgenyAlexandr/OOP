275a931d04650688817e3ad449e3d8bafa08f737
EvgenyAlexand <bragin-e@mail.ru>
2025-08-06 22:10:59 +0700

    HomeWork - In progress.

--- src/hogwarts.java
+++ src/hogwarts.java
@@ -1,7 +1,47 @@
+import java.util.Objects;
+
 public class hogwarts {
     private String name;
     private int magicPower;
     private int transgression;
 
+    // Конструктор класса
+    public hogwarts(String name, int magicPower, int transgression) {
+        this.name = name;
+        this.magicPower = magicPower;
+        this.transgression = transgression;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMagicPower() {
+        return magicPower;
+    }
+
+    public void setMagicPower(int magicPower) {
+        this.magicPower = magicPower;
+    }
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+    public void setTransgression(int transgression) {
+        this.transgression = transgression;
+    }
+
+    // Представление объекта
+    @Override
+    public String toString() {
+        return "Имя = '" + name + "' " +
+               ", Сила магии=" + magicPower +
+               ", Трансгрессия=" + transgression ;
+    }
 
 }
