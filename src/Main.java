497d2241fa78929e77eb6a941020e50dbebe5dff
EvgenyAlexand <bragin-e@mail.ru>
2025-08-06 22:39:45 +0700

    HomeWork - In progress.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         /* Название факультетов
             Гриффиндор — Gryffindor
-            Когтевран — Ravenclaw
-            Пуффендуй — Hufflepuff
-            Слизерин — Slytherin
+            Когтевран  — Ravenclaw
+            Пуффендуй  — Hufflepuff
+            Слизерин   — Slytherin
          */
 
 
