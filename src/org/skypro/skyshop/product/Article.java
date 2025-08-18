d6f95a8576b2088131b06e1efb1b23ed1b724d24
EvgenyAlexand <bragin-e@mail.ru>
2025-08-19 01:53:32 +0700

    HomeWork - Task 1 - In completed.

--- src/org/skypro/skyshop/product/Article.java
+++ src/org/skypro/skyshop/product/Article.java
@@ -1,6 +1,16 @@
 package org.skypro.skyshop.product;
 
 public class Article {
+    private final String articleName; 		// Название Статьи
+    private final String articleText;		// Текст статьи
 
+    public Article(String articleName, String articleText) {
+        this.articleName = articleName;
+        this.articleText = articleText;
+    }
 
+    public String toString() {
+        return "Название статьи: " + articleName + '\'' +
+               "Текст статьи: " + articleText + '\'';
+    }
 }
